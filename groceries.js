1fca51fd4a1763ba1201be70f5efe9e664068348
Will-Forest <wforest@mail.csuchico.edu>
2016-11-28 18:21:11 -0800

    Add files via upload
    x26- added save list/ delete list functions.

--- groceries.js
+++ groceries.js
@@ -1,5 +1,6 @@
 
 var myList = [];
+var listSet;
 //courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
@@ -22,7 +23,7 @@ function getCookie(cname) {
     }
     return "";
 }
-function addItem(){
+function addItem() {
 var input = document.getElementById("newItem").value;
 var list =  document.getElementById("listDisplay");
 var item = document.createElement("li");
@@ -37,17 +38,33 @@ btnClose.classList.add("btn-xs");
 btnClose.appendChild(iconClose);
 btnClose.addEventListener("click", removeParentListItem);
 if (myList.indexOf(input) == -1) {
-  item.appendChild(btnClose);
   item.appendChild(itemName);
+  item.appendChild(btnClose);
   list.appendChild(item);
   document.getElementById("newItem").value = "";
   myList.push(input);
   console.log(myList);
 }
-
 }
 function removeParentListItem() {
   var mom = this.parentNode;
   var grandma = mom.parentNode;
+  var itemRemove = mom.firstChild.textContent;
+  var itemIndex = myList.indexOf(itemRemove);
   grandma.removeChild(mom);
+  myList.splice(itemIndex, 1);
+  console.log(myList);
+
+}
+function saveList() {
+  //courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
+    listSet = myList.toString()
+  setCookie("groceries", listSet,31.00);
+}
+
+function clearList() {
+  var list =  document.getElementById("listDisplay");
+  list.innerHTML= "";
+  myList= [];
+  console.log(list)
 }
